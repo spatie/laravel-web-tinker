ca35cb334d77e8c45a1e22c3496e19e05e10607c
Razvan Aurariu <github@rzv.me>
2019-04-12 13:36:30 +0200

    removed comments in tests; created setup function to create the Tinker class only once; renamed the test function names to read like sentences

--- tests/TinkerTest.php
+++ tests/TinkerTest.php
@@ -6,52 +6,46 @@ use Spatie\WebTinker\Tinker;
 
 class TinkerTest extends TestCase
 {
-    /** @test */
-    public function remove_c_style_single_line_comments()
-    {
-        $tinker = new Tinker();
+    private $tinker;
 
-        // Only comment
-        $cleanCode = $tinker->removeComments('// This is a comment ');
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->tinker = new Tinker();
+    }
+    /** @test */
+    public function it_removes_c_style_single_line_comments()
+    {
+        $cleanCode = $this->tinker->removeComments('// This is a comment ');
         $this->assertSame('', $cleanCode);
 
-        // Comment at the end of line of code
-        $cleanCode = $tinker->removeComments('$user = \'Test\'; // This is a comment ');
+        $cleanCode = $this->tinker->removeComments('$user = \'Test\'; // This is a comment ');
         $this->assertSame('$user = \'Test\'; ', $cleanCode);
     }
 
     /** @test */
-    public function remove_shell_style_single_line_comments()
+    public function it_removes_shell_style_single_line_comments()
     {
-        $tinker = new Tinker();
-
-        // Only comment
-        $cleanCode = $tinker->removeComments('# This is a comment ');
+        $cleanCode = $this->tinker->removeComments('# This is a comment ');
         $this->assertSame('', $cleanCode);
 
-        // Comment at the end of line of code
-        $cleanCode = $tinker->removeComments('$user = \'Test\'; # This is a comment ');
+        $cleanCode = $this->tinker->removeComments('$user = \'Test\'; # This is a comment ');
         $this->assertSame('$user = \'Test\'; ', $cleanCode);
     }
 
     /** @test */
-    public function remove_php_multi_line_comments()
+    public function it_removes_php_multi_line_comments()
     {
-        $tinker = new Tinker();
-
-        // Only comment
-        $cleanCode = $tinker->removeComments("/* This is a multi line comment \n yet another line of comment */");
+        $cleanCode = $this->tinker->removeComments("/* This is a multi line comment \n yet another line of comment */");
         $this->assertSame('', $cleanCode);
 
-        // Multi line comment at the end of a line of code
-        $cleanCode = $tinker->removeComments(
+        $cleanCode = $this->tinker->removeComments(
             "\$user = 'Test User';/* This is a multi line comment \n".
             ' yet another line of comment */'
         );
         $this->assertSame('$user = \'Test User\';', $cleanCode);
 
-        // Inline Multi line comment
-        $cleanCode = $tinker->removeComments(
+        $cleanCode = $this->tinker->removeComments(
             "\$user = /* This is a multi line comment \n".
             " yet another line of comment */'Test User';"
         );
@@ -59,25 +53,20 @@ class TinkerTest extends TestCase
     }
 
     /** @test */
-    public function remove_comments_with_multiline_code_input()
+    public function it_removes_comments_with_multiline_code_input()
     {
-        $tinker = new Tinker();
-
-        // Multi line comment in between lines of code
-        $cleanCode = $tinker->removeComments(
+        $cleanCode = $this->tinker->removeComments(
             "\$user_id = 1; /* This is a multi line comment \n yet another line of comment */\n".
             '\App\User::find($user_id);'
         );
         $this->assertSame("\$user_id = 1; \n\\App\\User::find(\$user_id);", $cleanCode);
 
-        // C Style commented line of code
-        $cleanCode = $tinker->removeComments(
+        $cleanCode = $this->tinker->removeComments(
             "\$newUsers = User::latest()->take(3)->get();\n\n// \$allUsers = User::all();"
         );
         $this->assertSame("\$newUsers = User::latest()->take(3)->get();\n\n", $cleanCode);
 
-        // Shell Style commented line of code
-        $cleanCode = $tinker->removeComments(
+        $cleanCode = $this->tinker->removeComments(
             "\$newUsers = User::latest()->take(3)->get();\n\n# \$allUsers = User::all();"
         );
         $this->assertSame("\$newUsers = User::latest()->take(3)->get();\n\n", $cleanCode);
