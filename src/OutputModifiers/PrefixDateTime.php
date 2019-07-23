7f4a554f903347a28982cdeb8be76aa427ff4648
Alex Vanderbist <alex.vanderbist@gmail.com>
2019-07-23 16:56:14 +0200

    Actually include output in output

--- src/OutputModifiers/PrefixDateTime.php
+++ src/OutputModifiers/PrefixDateTime.php
@@ -6,6 +6,6 @@ class PrefixDateTime implements OutputModifier
 {
     public function modify(string $output = ''): string
     {
-        return '<span class="text-dimmed">'.now()->format('Y-m-d H:i:s').'</span><br>';
+        return '<span class="text-dimmed">'.now()->format('Y-m-d H:i:s').'</span><br>'.$output;
     }
 }
