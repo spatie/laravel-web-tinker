e64fa168521d23994e9b01e3a89943f3363b429f
scil <scil.zc@gmail.com>
2019-10-05 01:18:52 +0800

    take care of private prop when extending PsyShell

--- src/Shell/Shell.php
+++ src/Shell/Shell.php
@@ -2,21 +2,38 @@
 
 namespace Spatie\WebTinker\Shell;
 
+use Psy\Configuration;
+use Psy\Input\SilentInput;
 use Psy\Shell as PsyShell;
 
 class Shell extends PsyShell
 {
+    /**
+     * @var array  overwrite parent::$inputBuffer which is private
+     */
+    protected $inputBuffer;
+
+    public function __construct(Configuration $config = null)
+    {
+        parent::__construct($config);
+
+        $this->inputBuffer   = [];
+    }
+
+    public function addInput($input, $silent = false)
+    {
+        foreach ((array) $input as $line) {
+            $this->inputBuffer[] = $silent ? new SilentInput($line) : $line;
+        }
+    }
     protected function readline()
     {
-        if (!empty($this->inputBuffer)) {
-            $line = \array_shift($this->inputBuffer);
-            if (!$line instanceof SilentInput) {
-                $this->output->writeln(\sprintf('<aside>%s %s</aside>', static::REPLAY, OutputFormatter::escape($line)));
-            }
 
-            return $line;
+        if (!empty($this->inputBuffer)) {
+            return \array_shift($this->inputBuffer);
         }
 
         return false;
     }
+
 }
