be6f2dedf85beede027112dee630b391c8dd49ee
freek <freek@spatie.be>
2020-03-04 21:02:26 +0100

    wip

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to `laravel-web-tinker` will be documented in this file
 
+## 1.7.1 - 2020-03-04
+
+- fix install command in Laravel 7
+
 ## 1.7.0 - 2020-03-03
 
 - add support for Laravel 7
--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -3,12 +3,9 @@
 namespace Spatie\WebTinker\Console;
 
 use Illuminate\Console\Command;
-use Illuminate\Console\DetectsApplicationNamespace;
 
 class InstallCommand extends Command
 {
-    use DetectsApplicationNamespace;
-
     protected $signature = 'web-tinker:install';
 
     protected $description = 'Install all of the Web Tinker resources';
