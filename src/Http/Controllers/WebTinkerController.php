709404eacfeff8c29892d99a12444885b5a25b02
Karel Faille <shaffe.fr@gmail.com>
2020-03-04 16:29:14 +0100

    Can't execute code when running Laravel from subfolder

--- src/Http/Controllers/WebTinkerController.php
+++ src/Http/Controllers/WebTinkerController.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\WebTinker\Http\Controllers;
 
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Http\Request;
 use Spatie\WebTinker\Tinker;
 
@@ -9,7 +10,9 @@ class WebTinkerController
 {
     public function index()
     {
-        return view('web-tinker::web-tinker', ['path' => config('web-tinker.path')]);
+        return view('web-tinker::web-tinker', [
+            'path' => app(UrlGenerator::class)->to(config('web-tinker.path')),
+        ]);
     }
 
     public function execute(Request $request, Tinker $tinker)
