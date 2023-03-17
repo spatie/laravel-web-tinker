8007e829f772f78a295078a8fdcd572cc7aa1d97
vmartuniyk <martuniyk93@ukr.net>
2023-03-17 14:33:19 +0200

    refactor AuthorizeTest

--- tests/AuthorizeTest.php
+++ tests/AuthorizeTest.php
@@ -7,54 +7,6 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use Spatie\WebTinker\Http\Middleware\Authorize;
 
-//class AuthorizeTest extends TestCase
-//{
-//    public function setUp(): void
-//    {
-//        parent::setUp();
-//
-//        $this->actingAs(new User());
-//
-//        config()->set('web-tinker.enabled', true);
-//
-//        app()->detectEnvironment(function () {
-//            return 'local';
-//        });
-//
-//        Route::get('/test', function () {
-//            return 'ok';
-//        })->middleware(Authorize::class);
-//    }
-//
-//    /** @test */
-//    public function it_will_allow_requests_if_it_is_enabled()
-//    {
-//        $this->get('/test')->assertOk();
-//    }
-//
-//
-//    /** @test */
-//    public function it_will_not_allow_requests_if_the_gate_does_not_allow_it()
-//    {
-//        Gate::define('viewWebTinker', function () {
-//            return false;
-//        });
-//
-//        $this->get('/test')->assertStatus(403);
-//    }
-//
-//    /** @test */
-//    public function it_will_not_allow_requests_if_it_is_disabled_even_if_the_gate_allows_it()
-//    {
-//        Gate::define('viewWebTinker', function () {
-//            return true;
-//        });
-//
-//        config()->set('web-tinker.enabled', false);
-//
-//        $this->get('/test')->assertStatus(403);
-//    }
-//}
 beforeEach(function () {
 
     $this->actingAs(new User());
