8101442b244c247427e17f2daaa0706afe83daca
vmartuniyk <martuniyk93@ukr.net>
2023-03-17 14:32:10 +0200

    refactor AuthorizeTest

--- tests/AuthorizeTest.php
+++ tests/AuthorizeTest.php
@@ -7,50 +7,88 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Route;
 use Spatie\WebTinker\Http\Middleware\Authorize;
 
-class AuthorizeTest extends TestCase
-{
-    public function setUp(): void
-    {
-        parent::setUp();
+//class AuthorizeTest extends TestCase
+//{
+//    public function setUp(): void
+//    {
+//        parent::setUp();
+//
+//        $this->actingAs(new User());
+//
+//        config()->set('web-tinker.enabled', true);
+//
+//        app()->detectEnvironment(function () {
+//            return 'local';
+//        });
+//
+//        Route::get('/test', function () {
+//            return 'ok';
+//        })->middleware(Authorize::class);
+//    }
+//
+//    /** @test */
+//    public function it_will_allow_requests_if_it_is_enabled()
+//    {
+//        $this->get('/test')->assertOk();
+//    }
+//
+//
+//    /** @test */
+//    public function it_will_not_allow_requests_if_the_gate_does_not_allow_it()
+//    {
+//        Gate::define('viewWebTinker', function () {
+//            return false;
+//        });
+//
+//        $this->get('/test')->assertStatus(403);
+//    }
+//
+//    /** @test */
+//    public function it_will_not_allow_requests_if_it_is_disabled_even_if_the_gate_allows_it()
+//    {
+//        Gate::define('viewWebTinker', function () {
+//            return true;
+//        });
+//
+//        config()->set('web-tinker.enabled', false);
+//
+//        $this->get('/test')->assertStatus(403);
+//    }
+//}
+beforeEach(function () {
 
-        $this->actingAs(new User());
+    $this->actingAs(new User());
 
-        config()->set('web-tinker.enabled', true);
+    config()->set('web-tinker.enabled', true);
 
-        app()->detectEnvironment(function () {
-            return 'local';
-        });
+    app()->detectEnvironment(function () {
+        return 'local';
+    });
 
-        Route::get('/test', function () {
-            return 'ok';
-        })->middleware(Authorize::class);
-    }
+    Route::get('/test', function () {
+        return 'ok';
+    })->middleware(Authorize::class);
+});
 
-    /** @test */
-    public function it_will_allow_requests_if_it_is_enabled()
-    {
-        $this->get('/test')->assertOk();
-    }
+it('will allow request if it is enable', function () {
 
-    /** @test */
-    public function it_will_not_allow_requests_if_the_gate_does_not_allow_it()
-    {
-        Gate::define('viewWebTinker', function () {
-            return false;
-        });
+    $this->get('/test')->assertOk();
+});
+it('will not allow requests if the gate does not allow it', function(){
 
-        $this->get('/test')->assertStatus(403);
-    }
+    Gate::define('viewWebTinker', function () {
+        return false;
+    });
 
-    /** @test */
-    public function it_will_not_allow_requests_if_it_is_disabled_even_if_the_gate_allows_it()
-    {
-        Gate::define('viewWebTinker', function () {
-            return true;
-        });
+    $this->get('/test')->assertStatus(403);
+});
+it('will not allow requests if it is disabled even if the gate allows it', function (){
 
-        config()->set('web-tinker.enabled', false);
+    Gate::define('viewWebTinker', function () {
+        return true;
+    });
 
-        $this->get('/test')->assertStatus(403);
-    }
-}
+    config()->set('web-tinker.enabled', false);
+
+    $this->get('/test')->assertStatus(403);
+});
