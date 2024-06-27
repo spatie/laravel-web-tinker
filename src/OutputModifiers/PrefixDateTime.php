94a13f48312d3f824c616bd74252e87ca8505208
Niek den Breeje <n.denbreeje@guardian360.nl>
2024-06-27 16:00:08 +0200

    fix: prevent html injection
    Closes !106

--- src/OutputModifiers/PrefixDateTime.php
+++ src/OutputModifiers/PrefixDateTime.php
@@ -6,6 +6,8 @@ class PrefixDateTime implements OutputModifier
 {
     public function modify(string $output = ''): string
     {
+        $output = htmlentities($output);
+
         return '<span class="text-dimmed">'.now()->format('Y-m-d H:i:s').'</span><br>'.$output;
     }
 }
